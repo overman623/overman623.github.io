0e8f1d5a22bf5381ae8a29d9ec501de435198f4e
kimbeomjin <overman623@github.com>
2024-10-03 20:24:27 +0900

    새 코드 이식

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks"; // react-hooks 플러그인 추가
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    languageOptions: {
+      plugins: [pluginReactHooks], // react-hooks 플러그인 추가
+    },
+    rules: {
+      "react-hooks/exhaustive-deps": "warn", // 또는 'error'로 변경
+    },
+  },
+  {
+    // react 플러그인 설정
+    languageOptions: {
+      settings: {
+        react: {
+          version: "detect", // React 버전을 자동으로 감지
+        },
+      },
+    },
+    extends: [
+      "plugin:react/recommended",
+      "plugin:react-hooks/recommended", // react-hooks 추천 설정
+    ],
+  },
+];
